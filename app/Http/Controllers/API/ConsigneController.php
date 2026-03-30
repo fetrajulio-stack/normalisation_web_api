4a09cc9ac65ce067cef5feab14ddb07aabb483fc
MAMY TSILAVINA <mtsilavina@parc.lan>
2026-03-30 15:52:29 +0300

    modification requete champs

--- app/Http/Controllers/API/ConsigneController.php
+++ app/Http/Controllers/API/ConsigneController.php
@@ -221,7 +221,7 @@ class ConsigneController extends Controller
 
         $pdo = AccessService::connect($zCheminParametreMdb,null,null);
 
-        $sourceRows = $pdo->query(" SELECT idq FROM SOURCE")->fetchAll(PDO::FETCH_ASSOC);
+        $sourceRows = $pdo->query(" SELECT idq FROM LIVRAISON ORDER BY ordreq ASC")->fetchAll(PDO::FETCH_ASSOC);
 
         $sourceRows = $this->encodingService->utf8EncodeRecursive($sourceRows);
 
