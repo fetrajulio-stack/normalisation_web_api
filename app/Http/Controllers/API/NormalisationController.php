9dbdda71ba9a753e6a72f2a93a7a486478631dfb
Nanjaharifetra Julio RAKOTOARIMANANA <nanjah@parc.lan>
2026-05-22 13:56:48 +0300

    Teste 2

--- app/Http/Controllers/API/NormalisationController.php
+++ app/Http/Controllers/API/NormalisationController.php
@@ -663,6 +663,7 @@ public function importMdb(Request $request)
                     $rowsToProcess = AccessService::linuxQueryAssoc($filePath, 'Travail', $sqlTravail);
                 }
 
+                dd($rowsToProcess);
                 // === TRAITEMENT UNIFIE (Identique pour Windows et Linux) ===
                 foreach ($rowsToProcess as $row) {
                     
