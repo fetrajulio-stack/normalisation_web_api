c5dd481980ff82dea1b4dc4954912e22fb87778c
Nanjaharifetra Julio RAKOTOARIMANANA <nanjah@parc.lan>
2026-05-22 16:47:51 +0300

    Teste 3ds

--- app/Http/Controllers/API/NormalisationController.php
+++ app/Http/Controllers/API/NormalisationController.php
@@ -264,6 +264,7 @@ class NormalisationController extends Controller
             $sourceRows = $pdo->query($sql)->fetchAll(\PDO::FETCH_ASSOC);
         } else {
             $rawRows = AccessService::query($zCheminParametreMdb, $sql);
+            dd($rawRows);
             foreach ($rawRows as $rowLine) {
                 if($rowLine !== ""){
                     $parts = explode('||', $rowLine);
